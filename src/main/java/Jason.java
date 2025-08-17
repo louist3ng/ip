3cbd10c2a6a40e43e64f9105a1f0e7b762c5b66d
louist3ng <tenglouistjj@gmail.com>
2025-08-18 02:39:07 +0800

    Greet and Exit

--- src/main/java/Jason.java
+++ src/main/java/Jason.java
@@ -1,10 +1,11 @@
 public class Jason {
+
+    private static String name = "Jason";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("─".repeat(50));
+        System.out.println("Hello, my name is " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("─".repeat(50));
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
