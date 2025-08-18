07b3be2688573ff1c888bc02e69a71b7baa2c976
louist3ng <tenglouistjj@gmail.com>
2025-08-18 20:08:54 +0800

    Add Echo Support

--- src/main/java/Jason.java
+++ src/main/java/Jason.java
@@ -1,11 +1,30 @@
-public class Jason {
+import java.util.Scanner;
 
-    private static String name = "Jason";
+public class Jason {
+    private static String intro = "Hello, my name is Jason";
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String userInput = "";
+
         System.out.println("─".repeat(50));
-        System.out.println("Hello, my name is " + name);
-        System.out.println("What can I do for you?");
-        System.out.println("─".repeat(50));
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(intro);
+
+        while(!userInput.equalsIgnoreCase("bye")){
+            userInput = sc.nextLine();
+
+            if(userInput.equalsIgnoreCase("bye")){
+                System.out.println("─".repeat(50));
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("─".repeat(50));
+                break;
+            } else {
+                System.out.println("─".repeat(50));
+                System.out.println(userInput);
+                System.out.println("─".repeat(50));
+            }
+        }
+
+        sc.close();
     }
 }
